ce10d3b8efe336f71559c41bf8ab60252568dd81
CACUCRC <carloscu@hotmail.es>
2022-04-18 21:48:23 -0600

    estructura basica

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Chats from './Chats';
+import Grid from "@mui/material/Grid"
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { useState } from 'react';
 
 function App() {
+  const [logged, setLogged] = useState(true)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <Grid container direction="row" justifyContent="space-between" alignItems="center">
+          <h1>Chatter</h1>
+          <Tooltip title="Account management">
+            <IconButton>
+              <AccountCircleIcon fontSize='large' />
+            </IconButton>
+          </Tooltip>
+        </Grid>
       </header>
+      <body className="body">
+        {logged ? <Chats /> : <h1>Please log in</h1>}
+      </body>
     </div>
   );
 }
